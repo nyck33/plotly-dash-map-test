b5ecc2b9afca581c11f1659039b5cb8ed2fe86f7
nyck33 <nobutaka@gatech.edu>
2021-06-21 21:50:46 -0700

    added readme and random names

---
+++ readme.md
@@ -0,0 +1,44 @@
+# Plotly Dash Map Markers Demo
+
+I am still working on getting the coordinates outputted on zoom or drag 
+rather than on click on any spot on the map.  
+
+Plotly-Dash Leaflet 
+
+# Sources:
+# Plotly Dash Map Markers Demo
+
+I am still working on getting the coordinates outputted on zoom or drag 
+rather than on click on any spot on the map.  
+
+Plotly-Dash Leaflet (https://dash-leaflet.herokuapp.com/) is based on React Leaflet 
+but the documentation implies that not all features are implemented.
+
+# Instructions
+
+### 1. Please create a virtual environment for Python 3.8.8 with the requirements.txt
+(I used conda)
+command: pip install -r requirements.txt
+### 2. The program can be run to show coordinates next to a random name or the capital cities
+### For capital cities please run
+### command: python3 showTell1.py capitals
+or 
+### comamnd: python showTell1.py capitals
+### For random names no argument is required so please run:
+### command: python3 showTell1.py
+or
+### command: python showTell.py
+
+### 3. It should launch at 
+### http://127.0.0.1:8050/ 
+### or 
+### http://localhost:8050/
+
+### 4. Please click-drag and/or zoom the map and click somewhere on the map area.
+
+# Sources:
+
+https://dash-leaflet.herokuapp.com/#geojson
+https://community.plotly.com/t/show-and-tell-dash-leaflet/34924/18?u=nyck33
+https://dash-leaflet.herokuapp.com/#geojson
+https://community.plotly.com/t/show-and-tell-dash-leaflet/34924/18?u=nyck33
--- showTell1.py
+++ showTell1.py
@@ -1,3 +1,6 @@
+"""
+argv[1] capitals or random
+"""
 import json
 import dash
 import dash_html_components as html
@@ -5,6 +8,7 @@ from dash.dependencies import Output, Input, State
 import dash_leaflet as dl
 import pandas as pd
 import numpy as np
+import random, string, sys
 
 # Create example data frame.
 with open("assets/us-state-capitals.json") as f:
@@ -13,6 +17,8 @@ with open("assets/us-state-capitals.json") as f:
 capitals = []
 lats = []
 longs = []
+#also make a capitals dict
+capitals_d = {}
 for abbrev, d in usa_d.items():
     capital = d['capital']
     lat = float(d['lat'])
@@ -20,6 +26,12 @@ for abbrev, d in usa_d.items():
     capitals.append(capital)
     lats.append(lat)
     longs.append(long)
+    # make dict of tuple(lat, long) key: value: capital
+    lat_str = d['lat']
+    long_str = d['long']
+    #tup_key = (lat_str, long_str)
+    tup_key = (lat, long)
+    capitals_d[tup_key] = capital
 
 df = pd.DataFrame(columns=["lat", "lon"], data=np.column_stack((lats, longs)))
 
@@ -37,11 +49,11 @@ tile_layer = dl.TileLayer(url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
 layer_group = dl.LayerGroup(markers, id="layer-group")
 
 map = dl.Map(children=[dl.TileLayer(url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"), dl.LayerGroup(markers)],
-           style={'width': "100%", 'height': "90%"}, center=[34, -92], zoom=6, id="map"),
+           style={'width': "100%", 'height': "100%"}, center=[34, -92], zoom=6, id="map"),
 
 app.layout = html.Div([
     dl.Map(children=[tile_layer, layer_group],
-           style={'width': "100%", 'height': "100%"}, center=[34, -92], zoom=6, id="map"),
+           style={'width': "100%", 'height': "70%"}, center=[34, -92], zoom=6, id="map"),
     html.P("visible markers coordinates"),
     html.Div(id="coordinates"),
     html.P("click coordinates"),
@@ -64,7 +76,38 @@ def show_coordinates(click_loc, bounds, center, viewport, maxBounds, dragging, m
     for coord in click_loc:
         click_coord += str(coord)
         click_coord += " "
-    return "-", click_coord
+
+    #iterate markers list of dicts to get positions and if within bounds display
+    visible_coords_str = ""
+    visible_coords_arr = []
+    bounds_a, bounds_b = bounds
+    #todo: bottom right and top left?
+    # long is negative so reversed low and hi
+    lat_low, long_low = bounds_a
+    lat_hi, long_hi = bounds_b
+    for marker in markers:
+        data = marker['props']
+        pos = data['position']
+        lat, long = pos
+        if (lat_low <= lat <= lat_hi) and (long_low <= long <= long_hi):
+            # get capital
+            #capital = capitals_d[(str(lat), str(long))]
+            if len(sys.argv) > 1:
+                capital = capitals_d[(lat,long)]
+            else:
+                capital = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+            visible_coords_arr.append(capital + ": " + str(lat) + " " + str(long))
+
+    if len(visible_coords_arr) <= 0:
+        return "-", click_coord
+    #make a div
+    visible_div = html.Div([
+        html.P(
+            c
+        ) for c in visible_coords_arr
+    ])
+
+    return visible_div, click_coord
 
 
 '''
